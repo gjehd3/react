d1433c7adde3f932540be6b2e6dd6b7982041f11
Heo Dong <gjehd3@naver.com>
2020-08-24 22:47:54 +0900

    2.4.1.2 감싸인 요소 - 정상 작동
    부모 요소 하나로 요소 여러개를 감쌌다
    
    * 리액트 컴포넌트에서 요소를 왜 하나의 요소로 감싸야 하는가?
    -> Virtual DOM에서 컴포넌트 변화를 감지해 낼 때 효율적으로 비교할 수 있도록
    컴포넌트 내부는 하나의 DOM 트리 구조로 이루어져야 한다는 규칙이 있기 때문이다

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
-  )
+    <div>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </div>
+  );
 }
 
 export default App;
