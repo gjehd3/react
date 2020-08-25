30df58b4b91c37b781bde7c3b7773d6b1afe6180
Heo Dong <gjehd3@naver.com>
2020-08-25 20:05:23 +0900

    3.2.3.1 모듈 내보내기(export)
    예제 주석 참조

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -5,8 +5,5 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
-// 화살표 함수를 사용할 경우 this는 자신이 종속된 인스턴스를 가리킨다
-// 일반 함수의 경우 자신이 종속된 객체를 this로 가리킨다
-
-// 함수형 컴포넌트를 불러올 때 에디터에서 rsc를 입력하면 MyComponent를 불러올 수 있다
-// 클래스형 컴포넌트의 경우 rcc를 입력하면 된다
+/* 이 코드는 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent
+클래스를 불러오도록 설정한다 */
