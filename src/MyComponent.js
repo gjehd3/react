012305b26cb62fe6c33ead058035bb0ad2ec3c2a
Heo Dong <gjehd3@naver.com>
2020-08-25 20:03:06 +0900

    3.2.2.2 화살표 함수와 컴포넌트 불러오기
    코드의 주석 확인할 것

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -5,3 +5,8 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
+// 화살표 함수를 사용할 경우 this는 자신이 종속된 인스턴스를 가리킨다
+// 일반 함수의 경우 자신이 종속된 객체를 this로 가리킨다
+
+// 함수형 컴포넌트를 불러올 때 에디터에서 rsc를 입력하면 MyComponent를 불러올 수 있다
+// 클래스형 컴포넌트의 경우 rcc를 입력하면 된다
