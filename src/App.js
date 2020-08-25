7b6248d4a2b28463cfa036a3183035b2977409fc
Heo Dong <gjehd3@naver.com>
2020-08-25 19:40:57 +0900

    3.1.1 함수형 컴포넌트의 구조
    리액트를 이용하여 어플리케이션의 인터페이스를 설계할 때 사용자가 볼 수 있는
    요소는 여러 가지 컴포넌트로 구성된다
    
    컴포넌트를 선언하는 방식은 두가지이다
    1. 함수형 컴포넌트
    2. 클래스 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  const name = "리액트";
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다</p>
-    </div>
-  );
+  const name = '리액트';
+  return <div className="react">{name}</div>;
 }
 
 export default App;
