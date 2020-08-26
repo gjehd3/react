b86b9b57f0c7fe1e19b2efcf18281ca74ee2b28c
Heo Dong <gjehd3@naver.com>
2020-08-26 23:33:00 +0900

    6.1.2-2 map() 함수 - 예제(ES6 표현)

--- etc.js
+++ etc.js
@@ -1,7 +1,3 @@
 var numbers = [1, 2, 3, 4, 5];
-
-var processed = numbers.map(function (num) {
-  return num * num;
-});
-
-console.log(processed);
+const result = numbers.map((num) => num * num);
+console.log(result);
