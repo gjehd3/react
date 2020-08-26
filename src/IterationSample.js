446a1883ec1e03818f43eb1decfea041af770386
Heo Dong <gjehd3@naver.com>
2020-08-27 00:04:31 +0900

    6.4.3 데이터 제거 기능 구현하기
    각 항목을 더블 클릭했을 때 해당 항목이 사라지는 기능을 구현해보자
    불변성을 유지하고 업데이트 해야 한다
    
    불변성을 유지하면서 배열의 특정 항목을 지울 때는 배열 내장 함수 filter 사용
    
    filter 함수를 사용하면 배열에서 특정 조건을 만족하는 원소들만 제거 가능
    
    filter 함수 인자에 분류하고 싶은 조건을 반환 하는 함수를 넣어 주면 된다

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -21,7 +21,17 @@ const IterationSample = () => {
     setInputText(''); // inputText를 비움
   };
 
-  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
   return (
     <>
       <input value={inputText} onChange={onChange} />
