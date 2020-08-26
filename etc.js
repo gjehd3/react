29fe4bad90efbfd9f0b1c42dd5f2565185d71c61
Heo Dong <gjehd3@naver.com>
2020-08-26 22:31:04 +0900

    5.2.2 createRef를 통한 ref 설정
    ref를 만드는 또 다른 방법으로 리액트에 내장되어 있는 createRef 함수를 사용
    하는 것이 있다
    적은 코드로 쉽게 사용할 수 있지만 리액트 v16.3 이후 버전에서만 작동한다
    
    createRef를 사용하여 ref를 만들려면 컴포넌트 내부에서 멤버 변수로
    React.createRef()를 담아 주어야 한다
    해당 멤버 변수를 ref를 달고자 하는 요소에 ref props로 넣어주면 ref 설정이
    완료된다
    
    ref를 설정해 준 DOM에 접근하려면 this.input.current를 조회하면 된다
    -> 콜백 함수 사용법과 달리 뒤에 .current를 추가해야 한다

--- etc.js
+++ etc.js
@@ -1,5 +1,19 @@
-<input
-  ref={(ref) => {
-    this.input = ref;
-  }}
-/>;
+import React, { Component } from 'react';
+
+class RefSample extends Component {
+  input = React.createRef();
+
+  handleFocus = () => {
+    this.input.current.focus();
+  };
+
+  render() {
+    return (
+      <div>
+        <input ref={this.input} />
+      </div>
+    );
+  }
+}
+
+export default RefSample;
