202b851eee596f03ed94eb4cc6f0f69e8ae6d9f4
Heo Dong <gjehd3@naver.com>
2020-08-28 22:28:06 +0900

    8.6.1-1 로컬 변수 사용하기 - 클래스형 컴포넌트

--- hooks-tutorial/src/etc.js
+++ hooks-tutorial/src/etc.js
@@ -1,13 +1,16 @@
-useCallback(() => {
-    console.log(‘hello world!‘);
-  }, [])
-  
-  
-  useMemo(() => {
-    const fn = () => {
-      console.log(‘hello world!‘);
-    };
-    return fn;
-  }, [])
-  
-  
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+  id = 1;
+  setId = (n) => {
+    this.id = n;
+  };
+  printId = () => {
+    console.log(this.id);
+  };
+  render() {
+    return <div>MyComponent</div>;
+  }
+}
+
+export default MyComponent;
