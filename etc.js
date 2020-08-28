ba82704fcad15d7919b7c706239fa05ece402474
Heo Dong <gjehd3@naver.com>
2020-08-28 18:53:00 +0900

    7.2.3 getDerivedStateFromProps 메서드
    리액트 v16.3 이후에 새로 만들어진 라이프사이클 메서드이다
    
    업데이트, 마운트 카테고리에 속한다
    
    props로 받아 온 값을 state에 동기화시키는 용도로 사용하며, 컴포넌트가
    마운트될 때 업데이트될 때 호출한다

--- etc.js
+++ etc.js
@@ -1,1 +1,6 @@
-constructor(props) { ... }
+static getDerivedStateFromProps(nextProps, prevState) {
+    if(nextProps.value !== prevState.value) {
+        return { value: nextProps.value};
+    }
+    return null;
+}
