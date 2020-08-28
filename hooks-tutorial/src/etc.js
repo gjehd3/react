1bfec8677e27c5769e44c33e2952221759989c99
Heo Dong <gjehd3@naver.com>
2020-08-28 22:22:40 +0900

    8.5-2 useCallback - useMemo
    두 코드는 동일한 코드이다
    숫자 문자열 객체와 같은 일반 값을 재사용할 때 -> useMemo
    함수를 재사용할 때 -> useCallback

---
+++ hooks-tutorial/src/etc.js
@@ -0,0 +1,13 @@
+useCallback(() => {
+    console.log(‘hello world!‘);
+  }, [])
+  
+  
+  useMemo(() => {
+    const fn = () => {
+      console.log(‘hello world!‘);
+    };
+    return fn;
+  }, [])
+  
+  
