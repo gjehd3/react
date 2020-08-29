390adae060561041694b1dea27e7463a17368e78
Heo Dong <gjehd3@naver.com>
2020-08-30 02:35:43 +0900

    9.3.1-1 classnames
    classnames는 CSS 클래스를 조건부로 설정할 때 매우 유용한 라이브러리다
    CSS Module을 사용할 때 이 라이브러리를 사용하면 여러 클래스를 적용할 때
    매우 편리하다
    
    classnames의 기본적인 사용법이다

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,7 +1,12 @@
-$front-stack: Helvetica, sans-serif;
-$primary-color: #333;
+import classNames from ‘classnames’;
 
-body {
-    front:100% $front-stack;
-    color: $primary-color;
-}
+
+classNames(‘one’, ‘two’); // = ‘one two‘
+classNames(‘one’, { two: true }); // = ‘one two‘
+classNames(‘one’, { two: false }); // = ‘one‘
+classNames(‘one’, [‘two’, ‘three’]); // = ‘one two three‘
+
+
+
+const myClass = ‘hello’;
+classNames(‘one’, myClass, { myCondition: true }); // = ‘one hello myCondition‘
