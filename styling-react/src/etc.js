f13c55802ebbecc591103cc76ceaae81659eb433
Heo Dong <gjehd3@naver.com>
2020-08-30 03:09:53 +0900

    9.4.2-1 스타일링된 엘리먼트 만들기
    styled-components를 활용하여 스타일링된 엘리먼트를 만들 때는 컴포넌트
    파일의 상단에서 styled를 불러오고, styled.태그명을 사용하여 구현

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,4 +1,5 @@
-function tagged(...args) {
-  console.log(args);
-}
-tagged`hello ${{ foo: "bar" }} ${() => "world"}`;
+import styled from "styled-components";
+
+const MyComponent = styled.div`
+  font-size: 2rem;
+`;
