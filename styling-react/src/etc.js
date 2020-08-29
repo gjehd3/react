119b20263b49fa9019114c895c8821c34d5e7ae3
Heo Dong <gjehd3@naver.com>
2020-08-30 00:29:07 +0900

    9.2-2 Sass 사용하기 - .scss
    .sass 확장자는 중괄호와 세미콜론을 사용하지 않음
    node-sass 라이브러리는 Sass를 CSS로 변환한다

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,6 +1,7 @@
-$front-stack: Helvetica, sans-serif
-$primary-color: #333
+$front-stack: Helvetica, sans-serif;
+$primary-color: #333;
 
-body
-    front:100% $front-stack
-    color: $primary-color
+body {
+    front:100% $front-stack;
+    color: $primary-color;
+}
