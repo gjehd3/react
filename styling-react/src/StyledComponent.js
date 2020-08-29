570f70e254fdd7c3d8be7ec8cd5cdd11844cb5c5
Heo Dong <gjehd3@naver.com>
2020-08-30 03:20:40 +0900

    9.4.4 props에 따른 조건부 스타일링
    styled-components에서는 조건부 스타일링을 간단하게 props로도 처리할 수 있다
    
    <Button>안녕하세요</Button>
    <Button inverted={true}>테두리만</Button>
    
    스타일 코드 여러 줄을 props에 넣어 주어야 할 때는 CSS를 styled-components
    에서 불러와야 한다
    CSS를 사용하지 않고 문자열을 넣어도 자동하기는 한다
    -> 신택스 하이라이팅이 이루어지지 않는다
    -> Tagged 템플릿 리터럴이 아니기 때문에 함수를 받아 사용핮지 못해 해당 부분
    에서는 props 값을 사용하지 못한다
    
    props를 참조한다면 반드시 CSS로 감싸 주어서  Tagged 템플릿 리터럴을 사용해
    주어야 한다

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,12 +1,7 @@
-import React from "react";
 import styled, { css } from "styled-components";
-
-const Box = styled.div`
-  background: ${(props) => props.color || "blue"};
-  padding: 1rem;
-  display: flex;
-`;
-
+/* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는
+css를 불러와야 합니다. 
+*/
 const Button = styled.button`
   background: white;
   color: black;
@@ -40,12 +35,3 @@ const Button = styled.button`
     margin-left: 1rem;
   }
 `;
-
-const StyledComponent = () => (
-  <Box color="black">
-    <Button>안녕하세요</Button>
-    <Button inverted={true}>테두리만</Button>
-  </Box>
-);
-
-export default StyledComponent;
