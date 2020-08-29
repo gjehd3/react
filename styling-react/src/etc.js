b27d3d3baec11f983e520b316984456b07077a21
Heo Dong <gjehd3@naver.com>
2020-08-30 03:12:57 +0900

    9.4.2-2 스타일링된 엘리머트 만들기
    div로 이루어진 리액트 컴포넌트는 <MyComponent>Hello</MyComponent>와 같은
    형태로 사용할 수 있다
    
    div가 아닌 button이나 input에 스타일링을 하고 싶다면 styled.button 혹은
    styled.inpu 같은 형태로 뒤에 태그명을 넣어 주면 된다
    
    태그명이 유동적이거나 특정 컴포넌트에 스타일링해 주고 싶다면 다음과 같이 구현
    할 수 있다

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,5 +1,7 @@
-import styled from "styled-components";
+Const MyInput = styled('input')`
+background: gray;
+`
 
-const MyComponent = styled.div`
-  font-size: 2rem;
-`;
+const StyledLink = styled(Link)`
+color: blue;
+`
