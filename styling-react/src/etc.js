b1d9671fa18fb6eaf0b6be2cb5bbe66ef780b750
Heo Dong <gjehd3@naver.com>
2020-08-30 02:41:04 +0900

    9.3.1-3 classnames - 예시 코드2
    이런 식으로도 표현가능하나 classnames를 쓰는 것이 가독성이 높다
    
    CSS Module과 함께 사용하면 CSS Module 사용이 더 쉬워진다
    classnames에 내장되어 있는 bind 함수를 사용하면 클래스를 넣어 줄 때마다
    style.[클래스 이름] 형태를 사용할 필요가 없다

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,3 +1,5 @@
 const MyComponent = ({ highlighted, theme }) => (
-  <div className={classNames("MyComponent", { highlighted }, theme)}>Hello</div>
+  <div className={`MyComponent ${theme} ${highlighted ? "highlighted" : ""}`}>
+    Hello
+  </div>
 );
