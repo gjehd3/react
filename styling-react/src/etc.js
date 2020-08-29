f85e9775be495ad9a537210b07dc382f49d5c9c1
Heo Dong <gjehd3@naver.com>
2020-08-30 03:06:33 +0900

    9.4.1 Tagged 템플릿 리터럴
    문자열에 스타일 정보를 넣어 줄 때 `를 사용
    -> Tagged 템플릿 리터럴
    
    템플릿 안에 자바스크립트 객체나 함수를 전달할 때 온전히 추출할 수 있다
    
    위와 같이 function tagged를 추가하면 객체가 [object Object]로 변환되지
    않고 그대로 온전하게 출력된다

--- styling-react/src/etc.js
+++ styling-react/src/etc.js
@@ -1,8 +1,4 @@
-:local .wrapper {
-    /* 스타일 */
-    }
-    :local {
-    .wrapper {
-      /* 스타일 */
-    }
-    }
+function tagged(...args) {
+  console.log(args);
+}
+tagged`hello ${{ foo: "bar" }} ${() => "world"}`;
