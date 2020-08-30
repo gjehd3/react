3b5fca82a698962bdc638364fa3a25760e50193b
Heo Dong <gjehd3@naver.com>
2020-08-31 02:49:46 +0900

    12.1.5-2 useState의 함수형 업데이트와 immer 함께 쓰기
    immer에서 제공하는 produce 함수를 호출할 때, 첫번째 파라미터가 함수 형태라면
    업데이트 함수를 반환

--- immer-tutorial/etc.js
+++ immer-tutorial/etc.js
@@ -1,6 +1,9 @@
-const [number, setNumber] = useState(0);
-// prevNumbers는 현재 number 값을 가리킵니다.
-const onIncrease = useCallback(
-  () => setNumber((prevNumber) => prevNumber + 1),
-  []
-);
+const update = (draft) => {
+  draft.value = 2;
+};
+const originalState = {
+  value: 1,
+  foo: "bar",
+};
+const nextState = update(originalState);
+console.log(nextState); // { value: 2, foo: ‘bar‘ }
