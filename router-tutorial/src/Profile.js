d0cf795f130b6dbde16097bcbde7a34ff6b9957c
Heo Dong <gjehd3@naver.com>
2020-09-01 02:06:33 +0900

    13.5-1 서브 라우트
    라우트 내부에 또 라우트를 정의하는 것 -> 서브 라우트
    라우트로 사용되고 있는 컴포넌트 내부에 Route 컴포넌트를 또 사용하면 된다
    
    Profiles라는 라우트 컴포넌트를 만들고 Profile 컴포넌트를 서브 라우트로 사용

--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,35 +1,28 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Profile from './Profile';
 
-
-const data = {
-    velopert: {
-        name: '김민준',
-        description: '리액트를 좋아하는 개발자'
-    },
-    gildong: {
-        name: '홍길동',
-        description: '고전 소설 홍길동전의 주인공'
-    }
-};
-
-
-
-const Profile = ({ match }) => {
-    const { username } = match.params;
-    const profile = data[username];
-    if (!profile) {
-        return <div>존재하지 않는 사용자입니다.</div>;
-    }
+const Profiles = () => {
     return (
         <div>
-            <h3>
-                {username}({profile.name})
-      </h3>
-            <p>{profile.description}</p>
+            <h3>사용자 목록:</h3>
+            <ul>
+                <li>
+                    <Link to="/profiles/velopert">velopert</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/gildong">gildong</Link>
+                </li>
+            </ul>
+
+            <Route
+                path="/profiles"
+                exact
+                render={() => <div>사용자를 선택해 주세요.</div>}
+            />
+            <Route path="/profiles/:username" component={Profile} />
         </div>
     );
 };
 
-
-
-export default Profile;
+export default Profiles;
