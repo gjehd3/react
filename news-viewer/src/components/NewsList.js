f4ad2e52e6aa77e65617857275e6143f174ee24c
Heo Dong <gjehd3@naver.com>
2020-09-01 03:57:25 +0900

    14.5 데이터 연동하기
    useEffect를 사용하여 컴포넌트가 처음 렌더링되는 시점에 API를 요청
    
    useEffect에 등록하는 함수에 async를 붙이면 안됨
    -> useEffect에서 반환해야 하는 값이 뒷정리 함수 이기 때문
    -> useEffect 내부에서 async/await를 사용하고 싶다면 함수 내부에 async 키워드
    가 붙은 또 다른 함수를 만들어서 사용해야 한다
    
    뉴스 데이터 배열을 map 함수를 사용하여 컴포넌트 배열로 변환할 때 반드시
    !article을 조회하여 해당 값이 현재 null이 아닌지 검사해야 한다
    -> 데이터가 없을 때 null에는 map 함수가 없기 때문에 렌더링 과정에서 오류가
    발생-> 흰 페이지

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 
 const NewsListBlock = styled.div`
@@ -27,14 +28,41 @@ const sampleArticle = {
 
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'http://newsapi.org/v2/top-headlines?country=kr&apiKey=1dff0610147149da9758cbaa531976ed',
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    // 대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중…</NewsListBlock>;
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
@@ -42,3 +70,4 @@ const NewsList = () => {
 
 
 export default NewsList;
+
