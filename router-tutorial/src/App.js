56aa490ed77fbf5b04c2d70e9baec9300b3944b5
Heo Dong <gjehd3@naver.com>
2020-09-01 02:26:41 +0900

    13.6.1-2 history - App.js

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 import Profiles from './Profiles'
+import HistorySample from './HistorySample'
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route pate="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
 
     </div>
   )
