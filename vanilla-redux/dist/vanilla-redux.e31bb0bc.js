06788c6a6ffd426d89c7a71c5a04698c05178292
Heo Dong <gjehd3@naver.com>
2020-09-01 19:50:37 +0900

    16.2.8 render 함수 만들기
    render 함수는 상태가 업데이트될 때마다 호출되며, 리액트의 render 함수와 달리
    이미 html을 이용해 만들어진 UI의 속성을 상태에 따라 변경해 준다

--- vanilla-redux/dist/vanilla-redux.e31bb0bc.js
+++ vanilla-redux/dist/vanilla-redux.e31bb0bc.js
@@ -926,6 +926,22 @@ function reducer() {
 }
 
 var store = (0, _redux.createStore)(reducer);
+
+var render = function render() {
+  var state = store.getState(); // 현재 상태를 불러옵니다.
+  // 토글 처리
+
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  } // 카운터 처리
+
+
+  counter.innerText = state.counter;
+};
+
+render();
 },{"redux":"node_modules/redux/es/redux.js"}],"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
