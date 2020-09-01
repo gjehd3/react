00a20140f52a95dc67cbcb879502ebd54edd0889
Heo Dong <gjehd3@naver.com>
2020-09-01 15:42:19 +0900

    15.2.3-2 Provider - 오류 발생하는 경우

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -3,7 +3,7 @@ import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
 const App = () => {
   return (
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorContext.Provider>
       <div>
         <ColorBox />
       </div>
