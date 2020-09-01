cdfb0614a614fd002d72dcf2d5c76b20d8a36053
Heo Dong <gjehd3@naver.com>
2020-09-02 01:34:34 +0900

    18.3.1.5-7 리팩토링
    sample 리듀서에서 불필요한 코드를 지웠다

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -35,10 +35,6 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 
 const initialState = {
-    loading: {
-        GET_POST: false,
-        GET_USERS: false
-    },
     post: null,
     users: null
 };
@@ -47,50 +43,15 @@ const initialState = {
 
 const sample = handleActions(
     {
-        [GET_POST]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: true // 요청 시작
-            }
-        }),
         [GET_POST_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            },
             post: action.payload
-        }),
-        [GET_POST_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            }
-        }),
-        [GET_USERS]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: true // 요청 시작
-            }
         }),
         [GET_USERS_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            },
             users: action.payload
         }),
-        [GET_USERS_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            }
-        })
+
     },
     initialState
 );
