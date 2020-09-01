d3625f7388668aa7bfa6f48d666daed7b617bde6
Heo Dong <gjehd3@naver.com>
2020-09-01 16:26:00 +0900

    15.4.2-3 static contextType 사용하기
    static contextType을 정의하면 클래스 메서드에서도 Context에 넣어둔 함수를
    호출 가능하다
    그러나 한 클래스에 하나의 Context만 사용 가능 하다
    새로운 컴포넌트를 작성할 때 클래스형으로 작성하는 경우는 거의 없기 때문에
    useContext를 사용하는 것을 권장한다

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -5,29 +5,34 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 class SelectColors extends Component {
     static contextType = ColorContext;
+
+    handleSetColor = color => {
+        this.context.actions.setColor(color);
+    };
+
+    handleSetSubColor = subcolor => {
+        this.context.actions.setSubcolor(subcolor);
+    };
+
     render() {
         return (
             <div>
                 <h2>색상을 선택하세요.</h2>
-                <ColorConsumer>
-                    {({ actions }) => (
-                        <div style={{ display: 'flex' }}>
-                            {colors.map(color => (
-                                <div
-                                    key={color}
-                                    style={{ background: color, width: '24px', height: '24px', cursor: 'pointer' }}
-                                    onClick={() => actions.setColor(color)}
-                                    onContextMenu={e => {
-                                        e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
-                                        actions.setSubcolor(color);
-                                    }}
-                                />
-                            ))}
-                        </div>
-                    )}
-                </ColorConsumer>
+                <div style={{ display: 'flex' }}>
+                    {colors.map(color => (
+                        <div
+                            key={color}
+                            style={{ background: color, width: '24px', height: '24px', cursor: 'pointer' }}
+                            onClick={() => this.handleSetColor(color)}
+                            onContextMenu={e => {
+                                e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
+                                this.handleSetSubColor(color);
+                            }}
+                        />
+                    ))}
+                </div>
                 <hr />
-            </div>
+            </div >
         );
     }
 };
