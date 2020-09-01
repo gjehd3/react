083b28cb763cdf0bae30865bfe76be9fbbf76b17
Heo Dong <gjehd3@naver.com>
2020-09-01 23:12:26 +0900

    [오타 수정]

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react - redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
