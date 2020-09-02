110ac30fb82c119be17da4cd7269447c5976772f
Heo Dong <gjehd3@naver.com>
2020-09-02 20:34:18 +0900

    20.3.2-1 서버 사이드 렌더링 전용 웹팩 환경 설정 작성하기
    작성한 엔트리 파일을 웹팩으로 불러와서 빌드하려면 서버 전용 환경 설정을
    만들어 주어야 한다
    
    ssrIndexJs : 불러올 파일의 경로
    ssrBuild : 웹팩으로 처리한 뒤 결과물을 저장할 경로

--- ssr-recipe/config/paths.js
+++ ssr-recipe/config/paths.js
@@ -65,6 +65,8 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrlOrPath,
+  ssrIndexJs: resolveApp('src/index.server.js'), //서버 사이드 렌더링 엔트리
+  ssrBuild: resolveApp('dist'), //웹팩 처리 후 저장 경로
 };
 
 
