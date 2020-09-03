8a5a48dbb6ccd9636a7f9f244df1eb56ae0e6ac5
Heo Dong <gjehd3@naver.com>
2020-09-03 18:21:45 +0900

    20.4.5-2 스크립트로 스토어 초기 상태 주입하기[오타 수정]

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'edux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './modules';
