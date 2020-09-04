29b82592e3c00d497e16258273d5a7bc564fc067
Heo Dong <gjehd3@naver.com>
2020-09-05 00:41:56 +0900

    23.4.3-2 토큰 검증하기

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -5,6 +5,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -24,6 +25,8 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
+
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
