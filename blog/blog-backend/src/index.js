2bc216ed6bcaafe5a68a25c4c2b2ed9d827291aa
Heo Dong <gjehd3@naver.com>
2020-09-04 14:14:34 +0900

    21.5.2 라우트 파라미터와 쿼리
    파라미터와 쿼리는 둘 다 주소를 통해 특정 값을 받아 올 때 사용하지만, 용도가
    서로 조금씩 다릅니다. 정해진 규칙은 따로 없지만, 일반적으로 파라미터는 처리할
    작업의 카테고리를 받아 오거나, 고유 ID 혹은 이름으로 특정 데이터를 조회할 때
    사용합니다. 반면, 쿼리는 옵션에 관련된 정보를 받아 옵니다

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -8,10 +8,15 @@ const router = new Router();
 router.get('/', (ctx) => {
   ctx.body = '홈';
 });
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
 });
 
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다';
+});
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
