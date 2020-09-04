7f20996c941dd51b5793182d7ac0e9fcda683f25
Heo Dong <gjehd3@naver.com>
2020-09-04 19:13:50 +0900

    22.5.1-2 스키마 생성

---
+++ blog/blog-backend/example.js
@@ -0,0 +1,13 @@
+const AuthorSchema = new Schema({
+  name: String,
+  email: String,
+});
+const BookSchema = new Schema({
+  title: String,
+  description: String,
+  authors: [AuthorSchema],
+  meta: {
+    likes: Number,
+  },
+  extra: Schema.Types.Mixed,
+});
