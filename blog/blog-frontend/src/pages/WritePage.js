e92bd54e41a5936ae1038c50c67c8a2c92d00163
Heo Dong <gjehd3@naver.com>
2020-09-06 16:09:58 +0900

    25.3.3-5 글쓰기 API 연동하기
    WritePage에서 기존 WriteActionButtons 컴포넌트를 WriteAction
    ButtonsContainer로 대체

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
-import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
       <TagBoxContainer />
-      <WriteActionButtons />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
