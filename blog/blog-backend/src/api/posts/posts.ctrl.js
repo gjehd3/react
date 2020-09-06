4d2f9e0b883ec18c707e8fece47de9c35cfb43de
Heo Dong <gjehd3@naver.com>
2020-09-06 20:27:26 +0900

    26.2.3-5 HTML 필터링하기
    update 함수도 수정

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -162,8 +162,13 @@ export const update = async (ctx) => {
     ctx.body = result.error;
     return;
   }
+  const nextData = { ...ctx.request.body }; // 객체를 복사하고
+  // body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body);
+  }
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
     }).exec();
