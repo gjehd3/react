c5fecbe4e983ca3455e7a6a16768d92972d761e0
Heo Dong <gjehd3@naver.com>
2020-09-06 14:17:19 +0900

    25.3.2-2 TagBoxContainer 만들기

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EditorContainer from '../containers/write/EditorContainer';
-import TagBox from '../components/write/TagBox';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 
@@ -8,7 +8,7 @@ const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
-      <TagBox />
+      <TagBoxContainer />
       <WriteActionButtons />
     </Responsive>
   );
